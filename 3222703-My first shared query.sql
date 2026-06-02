295db5e7e05b9317416e184d49f6405321fe02f9
Tokollo Chiloane <tokollochief@gmail.com>
2026-06-02 23:06:56 +0000

    Update query `My first shared query`

--- 3222703-My first shared query.sql
+++ 3222703-My first shared query.sql
@@ -1,4 +1,4 @@
-ALTER TABLE patientvist MODIFY COLUMN visitdate DATE_FORMAT('')
+ALTER TABLE patientvist MODIFY COLUMN visitdate DATE_FORMAT('-%y')
 /* #########################################################                                */
 /* patientid,     comments,                                           visitdate,  diagnosis */
 /* ---------------------------------------------------------------------------------------- */
