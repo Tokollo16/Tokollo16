66b1fc3379ee7ef5fdec95ea0a54bd65d6eb7716
Tokollo Chiloane <tokollochief@gmail.com>
2026-06-03 02:28:25 +0000

    Update query `My first shared query`

--- 3222703-My first shared query.sql
+++ 3222703-My first shared query.sql
@@ -63,4 +63,4 @@ AND cancelled NOT IN ('Y')
 GROUP BY p.lastname, p.firstname, a.patientid
 HAVING COUNT(a.cancelled = 'N') > 2;
 
-select INITCAP(aller) from patientallergy;
+select RPAD(allergy,) from patientallergy;
