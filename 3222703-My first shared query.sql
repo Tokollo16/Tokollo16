791bd81abc089b5453fb86693f0269b787acead4
Tokollo Chiloane <tokollochief@gmail.com>
2026-06-03 02:23:38 +0000

    Update query `My first shared query`

--- 3222703-My first shared query.sql
+++ 3222703-My first shared query.sql
@@ -56,7 +56,7 @@ FROM patient p, respaccount r
 WHERE r.idnumber = p.resp;
 
 SELECT
-    CONCAT(p.lastname, ', ', SUBSTR(p.firstname, 1, 1), '(', a.patientid, ') ', COUNT(cancelled), ' appointment(s)') AS 'Cancelled appointment'
+    CONCAT(p.lastname, ', ', SUBSTR(p.firstname, 1, 1), '(', a.patientid, ') ', 'cancelled ', COUNT(cancelled), ' appointment(s)') AS 'Cancelled appointment'
 FROM patient p, appointment a
 WHERE p.patientid = a.patientid
 AND cancelled NOT IN ('Y')
