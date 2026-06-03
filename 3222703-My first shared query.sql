3efe1e7028049ad10b8f10dc045e5ebc494e145f
Tokollo Chiloane <tokollochief@gmail.com>
2026-06-03 00:58:22 +0000

    Update query `My first shared query`

--- 3222703-My first shared query.sql
+++ 3222703-My first shared query.sql
@@ -1,8 +1,8 @@
 SELECT 
-idnumber AS ClCode,
-lastname AS Surname,
-title AS Init,
-COALESCE(cellno, 'No Contact Number Provided') AS Contact_Status
+    idnumber AS ClCode,
+    lastname AS Surname,
+    title AS Init,
+    COALESCE(cellno, 'No Contact Number Provided') AS Contact_Status
 FROM respaccount
 WHERE substr(lastname, -1) = 'i'
 ORDER BY lastname;
