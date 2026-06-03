c3eb86c6aedc05162fe46b658a55ce3a288ed3a1
Tokollo Chiloane <tokollochief@gmail.com>
2026-06-03 02:14:30 +0000

    Update query `My first shared query`

--- 3222703-My first shared query.sql
+++ 3222703-My first shared query.sql
@@ -58,5 +58,5 @@ WHERE r.idnumber = p.resp;
 SELECT
     CONCAT(p.lastname, ', ', SUBSTR(p.firstname, 1, 1), '(', a.patientid, ')') AS 'Cancelled appointments'
 FROM patient p, appointment a
-WHERE p.patienid = a.patienid;
+WHERE p.patientid = a.patientid;
 #COUNT(a.cancelled = 'N') > 2;
