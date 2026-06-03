ef723de403d2d382acf6ea3f251dda72975b6cf2
Tokollo Chiloane <tokollochief@gmail.com>
2026-06-03 01:25:48 +0000

    Update query `My first shared query`

--- 3222703-My first shared query.sql
+++ 3222703-My first shared query.sql
@@ -10,12 +10,15 @@ ORDER BY lastname;
 
 
 
-
 SELECT
     r.idnumber AS ClCode,
     r.lastname AS Surname,
     COUNT(p.respaccount) AS Total_Payment,
     CASE
+        WHEN COUNT(p.respaccount) => 3 THEN 'VIP Payer'
+        WHEN COUNT(p.respaccount) BETWEEN 1 AND 2 THEN 'Standard Payer'
+        ELSE 'Inactive Account'
+    END AS Payment_Category
 FROM respaccount r
 JOIN payments p ON r.idnumber = p.respaccount
 GROUP BY r.idnumber, r.lastname
