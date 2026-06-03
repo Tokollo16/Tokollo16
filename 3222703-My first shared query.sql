367bb3ebbf2da0232edebd735a4be3ff241e9549
Tokollo Chiloane <tokollochief@gmail.com>
2026-06-03 01:45:00 +0000

    Update query `My first shared query`

--- 3222703-My first shared query.sql
+++ 3222703-My first shared query.sql
@@ -46,3 +46,10 @@ SELECT
     r.empname AS 'Responsible Party',
     r.address1 || ' ' || r.address2 AS 'Account City'
 FROM patient p, respaccount r;
+
+SELECT
+    p.patientid AS 'Patient ID',
+    CONCAT(p.firstname, ' ', p.lastname) AS 'Patient Name',
+    r.empname AS 'Responsible Party',
+    CONCAT(r.address1, ' ', r.address2) AS 'Account City'
+FROM patient p, respaccount r;
