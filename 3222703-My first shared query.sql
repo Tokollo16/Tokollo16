5f11909ee359cef0a9fb75a6a903998ebc93fd68
Tokollo Chiloane <tokollochief@gmail.com>
2026-06-03 04:23:06 +0000

    Update query `My first shared query`

--- 3222703-My first shared query.sql
+++ 3222703-My first shared query.sql
@@ -65,7 +65,7 @@ HAVING COUNT(a.cancelled = 'N') > 2;
 
 SELECT 
 CONCAT(RPAD(al.allergy, 13, '-'), '>') AS Allergy,
-RPAD(TO_CHAR(COUNT(pt.treatid)), 20) AS 'Treaments Linked',
+RPAD(TO_CHAR(COUNT(pt.treatid)), 20, ' ') AS 'Treaments Linked',
 treatmentdate AS 'Treatment Date' 
 FROM patientallergy pa, patienttreatment pt, allergylookup al
 GROUP BY pa.allergy, treatmentdate;
